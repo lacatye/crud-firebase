a3be19940fcf4a2d8b825847eda736946c46391f
Thalyta Diniz <thalytadiniz03@gmail.com>
2024-08-22 19:41:40 -0300

    feat: todo instance method and copy

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:crudfirebase/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -7,6 +8,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform);
+  FirebaseFirestore.instance.settings = const Settings(persistenceEnabled: true,);
   runApp(const MyApp());
 }
 
